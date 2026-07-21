ea36425a3c336fce5c6c9c1373159a7c59e7fb9d
bnadem39 <benali.adem39@esprit.tn>
2026-07-21 08:55:00 +0100

    0

---
+++ src/test/java/org/example/recrutment/RecrutmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.recrutment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecrutmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
